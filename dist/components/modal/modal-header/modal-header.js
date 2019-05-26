8e2bbce0bc08491f10c29c53346f090b377db792
Ben Johnson <weagle08@gmail.com>
2019-05-26 08:55:55 -0500

    no view model test

--- dist/components/modal/modal-header/modal-header.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    class ModalHeader {
-    }
-    exports.ModalHeader = ModalHeader;
-});
-
-//# sourceMappingURL=modal-header.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,13 @@
-define(["require", "exports", "aurelia-framework", "./components/modal/modal", "./components/modal/modal-body/modal-body", "./components/modal/modal-footer/modal-footer", "./components/modal/modal-header/modal-header"], function (require, exports, aurelia_framework_1, modal_1, modal_body_1, modal_footer_1, modal_header_1) {
+define(["require", "exports", "aurelia-framework", "./components/modal/modal", "./components/modal/modal-body/modal-body", "./components/modal/modal-footer/modal-footer"], function (require, exports, aurelia_framework_1, modal_1, modal_body_1, modal_footer_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.Modal = modal_1.Modal;
     exports.ModalBody = modal_body_1.ModalBody;
     exports.ModalFooter = modal_footer_1.ModalFooter;
-    exports.ModalHeader = modal_header_1.ModalHeader;
     function configure(config) {
         config.globalResources([
             aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal'),
-            aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal-header/modal-header'),
+            aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal-header/modal-header.html'),
             aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal-body/modal-body'),
             aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal-footer/modal-footer')
         ]);
