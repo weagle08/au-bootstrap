dc5811801343164a043b20bcf88b86d901a4f9df
Ben Johnson <weagle08@gmail.com>
2019-05-25 22:58:40 -0500

    initial modal

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
-let i = 0;
+define(["require", "exports", "aurelia-framework", "./components/modal/modal"], function (require, exports, aurelia_framework_1, modal_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Modal = modal_1.Modal;
+    function configure(config) {
+        config.globalResources([
+            aurelia_framework_1.PLATFORM.moduleName('./components/modal/modal')
+        ]);
+    }
+    exports.configure = configure;
+});
 
 //# sourceMappingURL=index.js.map
--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -3,25 +3,60 @@ import { bindable } from 'aurelia-framework';
 import { EventUtils } from '../../utils/EventUtils';
 import { ComponentBase } from '../ComponentBase';
 
+export interface IModalOptions {
+    backdrop: boolean | 'static';
+    keyboard: boolean;
+    focus: boolean;
+    show: boolean;
+}
+
 @autoinject
 export class Modal extends ComponentBase {
     @bindable public size: 'modal-sm' | 'modal-lg' | 'modal-xl' | undefined;
+    @bindable public options: IModalOptions;
 
     constructor(element: Element) {
         super(element);
     }
 
     public attached() {
+        this.jElement.modal(this.options);
+
+        this.jElement.on('show.bs.modal', () => {
+            EventUtils.fireEvent('show', this._domElement);
+        });
+
         this.jElement.on('shown.bs.modal', () => {
             EventUtils.fireEvent('shown', this._domElement);
         });
+
+        this.jElement.on('hide.bs.modal', () => {
+            EventUtils.fireEvent('hide', this._domElement);
+        });
+
+        this.jElement.on('hidden.bs.modal', () => {
+            EventUtils.fireEvent('hidden', this._domElement);
+        });
+
     }
 
     public show(): void {
-
+        this.jElement.modal('show');
     }
 
     public hide(): void {
+        this.jElement.modal('hide');
+    }
 
+    public toggle(): void {
+        this.jElement.modal('toggle');
+    }
+
+    public handleUpdate(): void {
+        this.jElement.modal('handleUpdate');
+    }
+
+    public dispose(): void {
+        this.jElement.modal('dispose');
     }
 }
