61f875531e20da965e411928b1d6e0e383df66c9
Ben <weagle08@gmail.com>
2020-01-18 11:07:16 -0600

    meh

--- src/components/tooltip/TooltipCustomAttribute.ts
+++ src/components/tooltip/TooltipCustomAttribute.ts
@@ -3,10 +3,42 @@ import { ComponentBase } from '../ComponentBase';
 
 @autoinject
 export class TooltipCustomAttribute extends ComponentBase {
-    @bindable public value: string;
+    public value: string;
 
     public constructor(element: Element) {
         super(element);
+    }
+
+    public show() {
+        this.jElement.tooltip('show');
+    }
+
+    public hide() {
+        this.jElement.tooltip('hide');
+    }
+
+    public toggle() {
+        this.jElement.tooltip('toggle');
+    }
+
+    public dispose() {
+        this.jElement.tooltip('dispose');
+    }
+
+    public enable() {
+        this.jElement.tooltip('enable');
+    }
+
+    public disable() {
+        this.jElement.tooltip('disable');
+    }
+
+    private bind() {
+        console.log('tooltip');
+        console.log(this.value);
+        this.jElement.tooltip({
+            title: 'hello world'
+        });
     }
 
     private attached() {
