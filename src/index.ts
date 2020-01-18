990cb9ef260b0c225881d10a49e5a56f2bf4aa75
Ben <weagle08@gmail.com>
2020-01-18 10:27:40 -0600

    Meh

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
-export { Modal, ModalEvents, IModalOptions } from './components/modal/modal';
+export * from './components/modal/modal';
+export * from './components/tooltip/TooltipCustomAttribute';
 
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName('./components/modal/modal'),
         PLATFORM.moduleName('./components/modal/modal-header/modal-header.html'),
         PLATFORM.moduleName('./components/modal/modal-body/modal-body.html'),
-        PLATFORM.moduleName('./components/modal/modal-footer/modal-footer.html')
+        PLATFORM.moduleName('./components/modal/modal-footer/modal-footer.html'),
+        PLATFORM.moduleName('./components/tooltip/TooltipCustomAttribute')
     ]);
 }
