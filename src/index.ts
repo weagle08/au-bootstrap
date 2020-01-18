93b0b3bc9a82bfa8182ec87ae6b964cfd5daccc1
Ben <weagle08@gmail.com>
2020-01-18 11:42:23 -0600

    meh

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
 
 export * from './components/modal/modal';
-export * from './components/tooltip/TooltipCustomAttribute';
 
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName('./components/modal/modal'),
         PLATFORM.moduleName('./components/modal/modal-header/modal-header.html'),
         PLATFORM.moduleName('./components/modal/modal-body/modal-body.html'),
-        PLATFORM.moduleName('./components/modal/modal-footer/modal-footer.html'),
-        PLATFORM.moduleName('./components/tooltip/TooltipCustomAttribute')
+        PLATFORM.moduleName('./components/modal/modal-footer/modal-footer.html')
     ]);
 }
